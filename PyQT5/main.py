5c55babea1088f732cb39a357fe2c63ce1942c72
Naptoss <tomgabinio@gmail.com>
2022-10-01 18:40:19 -0300

    main

--- PyQT5/main.py
+++ PyQT5/main.py
@@ -1,3 +1,15 @@
 import sys
 
-from 
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class App(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+if __name__ == '__main__':
+    qt = QApplication(sys.argv)
+    app = App()
+    app.show()
+    qt.exec_()
